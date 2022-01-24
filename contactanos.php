cd34c723ceac8d4d8c330a481890770353b46fda
BrianDLara <96392130+BrianDLara@users.noreply.github.com>
2022-01-23 21:48:06 -0500

    Update contactanos.php
    change e-mail address

--- contactanos.php
+++ contactanos.php
@@ -2,13 +2,16 @@
 
 if(isset($_POST['submit'])) {
     $name = $_POST['name'];
+    $subject = "Enviado de pagina web"
     $phone = $_POST['phone'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "96392130+BrianDLara@users.noreply.github.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "Haz recivido un e-mail de ".$name.".\n\n".$message;
-    mail($mailTo, $txt, $headers);
+    $mailTo = "dentalinnjl@gmail.com";
+    $headers = "From: ".$mailFrom. "\r\n" .
+    "CC: 96392130+BrianDLara@users.noreply.github.com";
+    $txt = "Haz recivido un e-mail de ".$name ."\r\nTelefono: " .$phone ."\r\n
+    Message: " .$message;
+    mail($mailTo, $subject, $txt, $headers);
     header("Location: contactanos.html?mailsend");
 }
